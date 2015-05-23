9a6d01c7eada24fa9093cb3947abd3e73352b258
Chris Gregg <tofergregg@gmail.com>
2015-05-23 17:27:33 -0400

    Added shift code to Arduino

--- SerialBeanArduino/SerialBeanArduino.ino
+++ SerialBeanArduino/SerialBeanArduino.ino
@@ -16,8 +16,8 @@ int pinValue = 0;
 void setup() {
   Serial.begin(57600); //open the serial port
   pinMode(LED, OUTPUT); 
-  Serial.println("Type b to start and s to stop blinking of the Arduino LED");
-  Serial.print(">"); //simulate prompt
+  //Serial.println("Type b to start and s to stop blinking of the Arduino LED");
+  //Serial.print(">"); //simulate prompt
   // set all pins to out
   for (int i=0;i<6;i++) {
     pinMode(i,OUTPUT);
@@ -32,98 +32,148 @@ void setup() {
 void loop() {
   
   inbyte = Serial.read(); //Read one byte (one character) from serial port.
-  if (inbyte == 'a') { 
+  if (inbyte != 255) {
+    Serial.write(inbyte);
+  }
+  
+  // if inbyte is a number between 1 and 48, we are going to shift a 1 to
+  // that position
+  // e.g., 1 would shift a 1 to position 0, and 10 would shift a 1 to position 11
+  if (inbyte <= 48) {
+    inbyte--; // just subtract one for actual value of shift
+    pinSet = -2; // don't do anything else after this is finished
+    // first clear
+    digitalWrite(3,0); // SRCLR low
+    //delay(40);
+    digitalWrite(3,1); // SRCLR high
+    //delay(40);
+    digitalWrite(A0,1); // RCK high
+    //delay(40);
+    digitalWrite(A0,0); // RCK low
+    //delay(40);
+    // shift a 1
+    digitalWrite(4,1); // SER IN high
+    //delay(40);
+    
+    digitalWrite(2,1); // SRCK high
+    //delay(40);
+    digitalWrite(2,0); // SRCK low
+    //delay(40);
+    digitalWrite(A0,1); // RCK high
+    //delay(40);
+    digitalWrite(A0,0); // RCK low
+    //delay(40);
+    
+    // shift inbyte number of 0s
+    digitalWrite(4,0); // SER IN low
+    //delay(40);
+
+    for (int i=0;i<inbyte;i++) {
+      digitalWrite(2,1); // SRCK high
+      //delay(40);
+      digitalWrite(2,0); // SRCK low
+      //delay(40);
+      digitalWrite(A0,1); // RCK high
+      //delay(40);
+      digitalWrite(A0,0); // RCK low
+      //delay(40);
+    }
+    Serial.write('Z');
+  }
+  else if (inbyte == 'a') { 
     // set SRCLR high
     pinSet = 3;
     pinValue = 1;
   }
   
-  if (inbyte == 'b') { 
+  else if (inbyte == 'b') { 
     // set SRCLR low
     pinSet = 3;
     pinValue = 0;
   }
   
-  if (inbyte == 'c') { 
+  else if (inbyte == 'c') { 
     // set G high
     pinSet = 5;
     pinValue = 1;
   }
   
-  if (inbyte == 'd') { 
+  else if (inbyte == 'd') { 
     // set G low
     pinSet = 5;
     pinValue = 0;
   }
   
-  if (inbyte == 'e') { 
+  else if (inbyte == 'e') { 
     // set RCK high
     pinSet = A0;
     pinValue = 1;
   }
   
-  if (inbyte == 'f') { 
+  else if (inbyte == 'f') { 
     // set RCK low
     pinSet = A0;
     pinValue = 0;
   }
 
-  if (inbyte == 'g') { 
+  else if (inbyte == 'g') { 
     // set SRCK high
     pinSet = 2;
     pinValue = 1;
   }
   
-  if (inbyte == 'h') { 
+  else if (inbyte == 'h') { 
     // set SRCK low
     pinSet = 2;
     pinValue = 0;
   }
 
-  if (inbyte == 'i') { 
+  else if (inbyte == 'i') { 
     // set SER IN high
     pinSet = 4;
     pinValue = 1;
   }
   
-  if (inbyte == 'j') { 
+  else if (inbyte == 'j') { 
     // set SER IN low
     pinSet = 4;
     pinValue = 0;
   }
   
-  if (inbyte == 'k') { 
+  else if (inbyte == 'k') { 
     // set SHIFT high
     pinSet = 1;
     pinValue = 1;
   }
   
-  if (inbyte == 'l') { 
+  else if (inbyte == 'l') { 
     // set SHIFT low
     pinSet = 1;
     pinValue = 0;
   }
   
-  if (inbyte == 'm') { 
+  else if (inbyte == 'm') { 
     // turn LED red
     pinSet = -1;
     pinValue = 1;
   }
   
-  if (inbyte == 'n') { 
+  else if (inbyte == 'n') { 
     // turn LED off
     pinSet = -1;
     pinValue = 0;
   }
   
-  if (inbyte >= 'a' && inbyte <= 'n') { 
+  /*if (inbyte >= 'a' && inbyte <= 'n') { 
     Serial.println(inbyte); //echo the command
     Serial.print(">"); 
-  }
-  if (pinSet != -1) {
+  }*/
+  if (pinSet >= 0) {
     digitalWrite(pinSet,pinValue);
+    Serial.write('X');
+    pinSet = -2;
   }
-  else {
+  else if (pinSet == -1) {
     // must be LED
     if (pinValue == 0) {
         Bean.setLed(0,0,0);
@@ -132,6 +182,7 @@ void loop() {
         Bean.setLed(255,0,0);
     }
   }
-  Bean.sleep(200);
+  //Bean.sleep(50);
+  delay(1);
 }
 
